10a96435ce55d7db19415e0375072bd6afc3b620
Paweł Król <pawel.krol89@gmail.com>
2021-08-26 16:58:58 +0200

    add remark

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function titleClickHandler(event){
   /* find the correct article using the selector (value of 'href' attribute) */
 
   const targetArticle = document.querySelector(articleSelector);
-  console.log (targetArticle);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
 
